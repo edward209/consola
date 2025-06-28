c1cffed7501e242895b2f9bd54bfaf32e65bbc87
edward209 <franciscoedward296@gmail.com>
2025-06-28 14:27:10 -0400

    Ejercicios realizado

---
+++ A3Ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://using System;
+
+namespace SumaConFuncion
+{
+    class Program
+    {
+        static int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer número:");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            int Num2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = Sumar(Num1, Num2);
+
+            Console.WriteLine($"La suma de {Num1} y {Num2} es: {resultado}");
+        }
+    }
+}
+
